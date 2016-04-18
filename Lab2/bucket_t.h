ef709e986fad20009a82c1c32fd3e6dcdf4baca6
huffSamuel <samueljhuff@gmail.com>
2016-04-17 21:06:53 -0700

    Final commit before turning in

--- Lab2/bucket_t.h
+++ Lab2/bucket_t.h
@@ -3,16 +3,25 @@
 #include <unistd.h>
 #include <stdint.h>
 
+// Detailed information on functions found in bucket_t.c
+
+// Structure for storing memory chunk information
 typedef struct
 {
   short m_size;         // Number of bytes for addresses in this bucket
-  uint8_t m_count;       // Number of this order used. Max is 64 16 byte fields
+  uint8_t m_count;      // Number of this order used. Max is 64 16 byte fields
   char * m_offset;      // True offset is calculated by m_offset << 4
 } bucket_t;
 
+// Adds a memory value to a bucket
 void Add(bucket_t * bucket, int size, int addr);
+// Moves a memory value from one bucket to another
 void Move(bucket_t * dest, bucket_t * source, int order, int offset);
+// Joins two memory values
 intptr_t Join(int order, int offsetA, int offsetB);
+// Splits a memory value of requested size
 intptr_t split(int size);
+// Coalesces two memory value into a larger one
 void coalesce(int addr, int size);
+// Remoes a memory value from an order
 void removeOrder(int order, int a, int b, int srcCount);
--- Lab2/buddy.h
+++ Lab2/buddy.h
@@ -1,20 +1,29 @@
 #include <stdlib.h>
 #include "bucket_t.h"
 
+// Detailed information on functions found in buddy.c
+
+// Initializes 2048 memory chunk and free/busy lists
 void my_mem_init();
+// Deallocates all memory used
 void my_mem_cleanup();
+// Emulates functionality of malloc()
 void * my_malloc(int size);
+// Prints the current status of memory
 void my_print_mem();
+// Emulates functionality of free()
 void my_free(void * ptr);
+// Validates memory to check for data corruption
 void my_validate();
 
 // Helper functions
+// Rounds a size up to the nearest base-2 value
 int roundUp(int size);
+// Computes the order of a given size
 int computeOrder(int size);
+// Finds the buddy address of an address and size
 int findBuddy(int addr, short size);
+// Prints detailed memory values from a given list
 int printInList(int addr, int order, int listID);
+// Prints the contents of free memory
 void printFree();
-int printBusy(int addr, int order);
-
-void coalesce(int addr, int size);
-intptr_t allocate(int size);
