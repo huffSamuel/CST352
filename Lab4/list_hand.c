cedda1fcb5799dd8f89f3be993997b35ee93181a
huffSamuel <samueljhuff@gmail.com>
2016-05-05 22:14:42 -0700

    Error in the traversal of list_hand:list_sorted_insert

--- Lab4/list_hand.c
+++ Lab4/list_hand.c
@@ -8,6 +8,7 @@
 
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 #include "list_hand.h"
 
@@ -46,65 +47,97 @@ list_t *list_init()
 ************************************************************************/
 void list_sorted_insert(list_t *list, int value)
 {
+    printf("Insert %d\n", value);
+    fflush(stdout);
     list_item_t *item;
 
     assert(list != NULL);
 
     list_item_t *new_item = (list_item_t *)malloc(sizeof(list_item_t));
     assert(new_item != NULL);
-    pthread_mutex_init(&(new_item->lock), NULL);
 
     new_item->value = value;
+    new_item->next = NULL;
 
+    // Lock list
     pthread_mutex_lock(&(list->lock));
-    if(list->first != NULL) 
-    {
+    if(list->count != 0)    // Lock first if needed
         pthread_mutex_lock(&(list->first->lock));
-    }
-
-    if (list->first == NULL || value < list->first->value) 
+    
+    // List locked
+    // List first NOT locked
+    if(list->count == 0)
     {
-        // special case inserting at beginning of list
-        
+        pthread_mutex_lock(&(new_item->lock));
+        new_item->next = NULL;
+        list->first = new_item;
+        list->last = new_item;
+        pthread_mutex_unlock(&(list->lock));
+        pthread_mutex_unlock(&(new_item->lock));
+    }
+    
+    // List->first is locked
+    // List is locked
+    else if (value < list->first->value) 
+    {
         pthread_mutex_lock(&(new_item->lock));
         new_item->next = list->first;
-        //pthread_mutex_lock(&(list->lock));
         list->first = new_item;
-        //pthread_mutex_unlock(&(list->lock));
-        pthread_mutex_unlock(&(new_item->lock));
-
-        if(new_item->next != NULL)
-            pthread_mutex_unlock(&(new_item->next->lock));
-
-        if (list->last == NULL) 
-        {
-            //pthread_mutex_lock(&(list->lock));
-            list->last = new_item;
-            //pthread_mutex_unlock(&(list->lock));
-        }
         pthread_mutex_unlock(&(list->lock));
+        pthread_mutex_unlock(&(new_item->next->lock));   // Unlocked the previous list->first
+        pthread_mutex_unlock(&(list->first->lock));
     }
+
+    // List->first is locked
+    // List is locked
+    else if(list->count == 1)
+    {
+        pthread_mutex_lock(&(new_item->lock));
+        list->first->next = new_item;
+        pthread_mutex_unlock(&(list->first->lock)); // Unlock first
+        list->last = new_item;
+        pthread_mutex_unlock(&(new_item->lock));
+        pthread_mutex_unlock(&(list->lock)); // Unlock list
+    }
+
+    // List->first is locked
+    // List is locked
     else
     {
         item = list->first;
+        if(list->count > 1)
+            pthread_mutex_lock(&(item->next->lock));
+
         pthread_mutex_unlock(&(list->lock));
+
+        // Entering:
+        // List is unlocked
+        // List->first is locked
+        // list->first->next is locked
         while (item->next != NULL && item->next->value < value)
         {
-            pthread_mutex_lock(&(item->next->lock));
             pthread_mutex_unlock(&(item->lock));
-            item = item->next;
+            item = item->next; 
+            if(item->next != NULL)
+                pthread_mutex_lock(&(item->next->lock));
         }
 
+        // item is locked
+        // item->next is locked
+        pthread_mutex_lock(&(new_item->lock));  // Lock new
         new_item->next = item->next;
         item->next = new_item;
-        
+        if(new_item->next != NULL)
+            pthread_mutex_unlock(&(new_item->next->lock)); // Unlock item->next
+
         if (item->next == NULL) 
         {
             pthread_mutex_lock(&(list->lock));
             list->last = new_item;
             pthread_mutex_unlock(&(list->lock));
         }
-        pthread_mutex_unlock(&(item->lock));
+        pthread_mutex_unlock(&(new_item->lock));    // unlock new
+        pthread_mutex_unlock(&(item->lock));        // Unlock item
     }
 
     pthread_mutex_lock(&(list->lock));
